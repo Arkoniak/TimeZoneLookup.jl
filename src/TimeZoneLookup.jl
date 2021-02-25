cab74b064eb5a081d10bb6ee892f731321074010
Andrey Oskin <skoffer@arkoniak.com>
2021-02-25 12:57:52 +0200

    Broken commit, reintroduction of the left adj

--- src/TimeZoneLookup.jl
+++ src/TimeZoneLookup.jl
@@ -2,10 +2,11 @@ module TimeZoneLookup
 
 using Shapefile
 using Setfield
-import Base: isless, zero, similar
+using AbstractTrees
+import Base: isless, zero, similar, show
 
 export buildedges, mergeedges, buildedges!, buildmega, boundingbox, TrapezoidalSearchNode, TrapezoidData, query, Trapezoid, E, V
-export faceof, EData, lhs, rhs, lp, rp, update!
+export faceof, EData, lhs, rhs, lp, rp, update!, sideof
 
 const PRECEDENCE = [
  ("Asia/Hebron", "Asia/Jerusalem"),
@@ -27,17 +28,26 @@ struct V{T}
     y::T
 end
 zero(::Type{V{T}}) where T = V(zero(T), zero(T))
+function show(io::IO, v::V)
+    print(io, "{", v.x, ", ", v.y, "}")
+end
 
 struct E{T}
     p::V{T}
     q::V{T}
 end
 zero(::Type{E{T}}) where T = E(zero(V{T}), zero(V{T}))
+function show(io::IO, e::E)
+    print(io, e.p, "→", e.q)
+end
 
 struct EData{T}
     e::E{T}
     l::Int # Left hand face
     r::Int # Right hand face
+end
+function show(io::IO, ed::EData)
+    print(io, "Edge: ", ed.e, ", ", ed.l, ", ", ed.r)
 end
 
 lhs(x::EData) = x.l
@@ -45,57 +55,82 @@ rhs(x::EData) = x.r
 edge(x::EData) = x.e
 lp(x::EData) = edge(x).p
 rp(x::EData) = edge(x).q
+sideof(x::EData, r::V) = sideof(edge(x), r)
 
-function faceof(x::EData, r::V)
-    e = x.e
-    p = e.p
-    q = e.q
-    d1 = q.x - p.x
-    d2 = q.y - p.y
-    d3 = p.x*q.y - p.y*q.x
-    ld = d1*r.y - d2*r.x
-    ld > d3 ? lhs(x) : rhs(x)
-end
+faceof(x::EData, r::V) = sideof(x, r) ? lhs(x) : rhs(x)
 
 function sideof(x::E, r::V)
     p = x.p
     q = x.q
     d1 = q.x - p.x
     d2 = q.y - p.y
-    d3 = p.x*q.y - p.y*q.x
-    ld = d1*r.y - d2*r.x
+    d3 = p.y * q.x - p.x * q.y
+    ld = d1 * r.y - d2 * r.x
+    # @info "Sideof" p q d1 d2 d3 ld
     return ld > d3
 end
 
 isless(e1::E{T}, e2::E{T}) where T = isless((e1.p, e1.q), (e2.p, e2.q))
 isless(v1::V{T}, v2::V{T}) where T = isless((v1.x, v1.y), (v2.x, v2.y))
 
-struct Trapezoid{T}
-    leftp::V{T}
-    rightp::V{T}
-    top::E{T}
-    bottom::E{T}
-    face::Int
-end
-
-abstract type AbstractTrapezoidData end
-mutable struct TrapezoidalSearchNode{T, T1 <: AbstractTrapezoidData} 
+abstract type AbstractTrapezoid end
+mutable struct TrapezoidalSearchNode{T, T1 <: AbstractTrapezoid} 
     left::Union{TrapezoidalSearchNode{T, T1}, Nothing}
     right::Union{TrapezoidalSearchNode{T, T1}, Nothing}
     condition::Tuple{Int, E{T}}
     data::Union{T1, Nothing}
 end
-
-mutable struct TrapezoidData{T} <: AbstractTrapezoidData
-    t::Trapezoid{T}
-    rb::Union{TrapezoidData{T}, Nothing} # Right bottom adjacent trapezoid
-    rt::Union{TrapezoidData{T}, Nothing} # Right top adjacent trapezoid
-    node::Union{TrapezoidalSearchNode{T, TrapezoidData{T}}, Nothing}    # Node where this trapezoid can be found
+function show(io::IO, node::TrapezoidalSearchNode)
+    print(io, node.data, "; ")
+    if node.condition[1] == 0
+        print(io, "Leaf")
+    elseif node.condition[1] == 1
+        print(io, "Point: ", node.condition[2].p)
+    else
+        print(io, "Edge:  ", node.condition[2])
+    end
 end
 
-TrapezoidalSearchNode(data::TrapezoidData{T}) where T = TrapezoidalSearchNode{T, TrapezoidData{T}}(nothing, nothing, (0, zero(E{T})), data)
-TrapezoidData(t::Trapezoid{T}) where T = TrapezoidData(t, nothing, nothing, nothing)
+mutable struct Trapezoid{T} <: AbstractTrapezoid
+    leftp::V{T}
+    rightp::V{T}
+    top::E{T}
+    bottom::E{T}
+    face::Int
+    lb::Union{Trapezoid{T}, Nothing} # Left bottom adjacent trapezoid
+    lt::Union{Trapezoid{T}, Nothing} # Left top adjacent trapezoid
+    rb::Union{Trapezoid{T}, Nothing} # Right bottom adjacent trapezoid
+    rt::Union{Trapezoid{T}, Nothing} # Right top adjacent trapezoid
+    node::Union{TrapezoidalSearchNode{T, Trapezoid{T}}, Nothing}    # Node where this trapezoid can be found
+end
+Trapezoid(leftp::V{T}, rightp::V{T}, top::E{T}, bottom::E{T}, face) where T = Trapezoid(leftp, rightp, top, bottom, face, nothing, nothing, nothing, nothing, nothing)
 
+TrapezoidalSearchNode(data::Trapezoid{T}) where T = TrapezoidalSearchNode{T, Trapezoid{T}}(nothing, nothing, (0, zero(E{T})), data)
+
+function show(io::IO, t::Trapezoid)
+    print(io, "Trapezoid: ", t.leftp, ", ", t.rightp, ", ", t.top, ", ", t.bottom, ", ", t.face)
+end
+
+# For better tree printing and navigation
+## Things we need to define
+function AbstractTrees.children(node::TrapezoidalSearchNode)
+    if node.left === nothing
+        if node.right === nothing
+            return ()
+        else
+            return (node.right, )
+        end
+    else
+        if node.right === nothing
+            return (node.left, )
+        else
+            return (node.left, node.right)
+        end
+    end
+end
+
+## Things that make printing prettier
+AbstractTrees.printnode(io::IO, node::TrapezoidalSearchNode) = print(io, node)
 
 function buildmega(table)
     s = 0
@@ -247,39 +282,64 @@ function query(node::TrapezoidalSearchNode, p)
     node.condition[1] == 0 && return node.data
     if node.condition[1] == 1
         if p.x < node.condition[2].p.x
+            # println("Point left: ", node.condition[2].p)
             return query(node.left, p)
         else
+            # println("Point right: ", node.condition[2].p)
             return query(node.right, p)
         end
     else
         if sideof(node.condition[2], p)
+            # println("Edge left: ", node.condition[2])
             return query(node.left, p)
         else
+            # println("Edge right: ", node.condition[2])
             return query(node.right, p)
         end
     end
 end
 
-function buildnode(t)
-    td = TrapezoidData(t)
-    tn = TrapezoidalSearchNode(td)
-    td.node = tn
+function buildnode(leftp, rightp, top, bottom, face)
+    t = Trapezoid(leftp, rightp, top, bottom, face)
+    node = TrapezoidalSearchNode(t)
+    t.node = node
 
-    return td, tn
+    return t, node
+end
+
+function buildnode(t)
+    node = TrapezoidalSearchNode(t)
+    t.node = node
+
+    return t, node
 end
 
 similar(node::TrapezoidalSearchNode{T, T1}) where {T, T1} = TrapezoidalSearchNode{T, T1}(nothing, nothing, (0, zero(E{T}), nothing))
 
+function findadjacent(s, t)
+    # If we have only one adjacent trapezoid, return it
+    t.rb === nothing && return t.rt
+    t.rt === nothing && return t.rb
+
+    # We have two adjacent trapezoids, must choose one of them
+    p = lp(s)
+    if t.rightp == p
+        return sideof(s, t.rb.top.q) ? t.rb : t.rt
+    end
+
+    # General position
+    return sideof(s, t.rightp) ? t.rt : t.rb
+end
+
 function update!(tree::TrapezoidalSearchNode, s)
-    td = query(tree, lp(s))
-    t = td.t
+    t = query(tree, lp(s))
     p = lp(s)
     q = rp(s)
     e = edge(s)
 
-    node = td.node
+    node = t.node
     prevnodeexists = false
-    tdprev = td
+    tprev = t
 
     if p.x > t.leftp.x && p.x < t.rightp.x
         # Transforming node to internal and adding type 1 condition
@@ -288,123 +348,120 @@ function update!(tree::TrapezoidalSearchNode, s)
         node.condition = (1, E(p, p))
 
         # Split initial trapezoid and add results to the left and right nodes
-        t1 = Trapezoid(t.leftp, p, t.top, t.bottom, t.face)
-        td1, tn1 = buildnode(t1)
-        node.left = tn1
+        # Fake right trapezoid, just to simplify things
+        t2, n2 = buildnode(p, t.rightp, t.top, t.bottom, t.face)
+        t2.rb = t.rb
+        t2.rt = t.rt
+        node.right = n2
 
-        t2 = Trapezoid(p, t.rightp, t.top, t.bottom, t.face)
-        td2, tn2 = buildnode(t2)
-        td2.rb = td.rb
-        td2.rt = td.rt
-        node.right = tn2
+        t.rightp = p
+        t, n1 = buildnode(t)
+        node.left = n1
 
-        # Fake left trapezoid, just to simplify things
         prevnodeexists = true
-        node = tn2
-        td = td2
+        node = n2
         t = t2
     elseif p.x == t.rightp.x
-        # Searching right adjacent node
-        if sideof(s, t.rightp)
-            td = td.rt
-        else
-            td = td.rb
-        end
-
-        # It shouldn't happen though
-        if td === nothing
-            return nothing
-        end
+        t = findadjacent(s, t)
         prevnodeexists = true
-        node = td.node
-        t = td.t
+        node = t.node
     end
 
     splitting = true
     istopactive = false
     isbotactive = false
+    local topt, bott, topn, botn
     while splitting
-        # TODO: what if t.rightp.x == q.x?
         if t.rightp.x > q.x
             splitting = false
             # Introduce rightmost node and split trapezoid vertically
-            t1 = Trapezoid(q, t.rightp, t.top, t.bottom, t.face)
-            td1, tn1 = buildnode(t1)
-            td1.rt = td.rt
-            td1.rb = td.rb
-            node.right = tn1
+            t1, n1 = buildnode(q, t.rightp, t.top, t.bottom, t.face)
+            t1.rt = t.rt
+            t1.rb = t.rb
+            node.right = n1
 
-            t2 = Trapezoid(t.leftp, q, t.top, t.bottom, t.face)
-            td2, tn2 = buildnode(t2)
-            node.left = tn2
+            t2, n2 = buildnode(t.leftp, q, t.top, t.bottom, t.face)
+            node.left = n2
 
             node.data.node = nothing
             node.data = nothing
             node.condition = (1, E(q, q))
 
-            tdprev = td1
+            tprev = t1
             t = t2
-            td = td2
-            node = tn2
+            node = n2
+        end
+
+        # TODO: what if t.rightp.x == q.x? Is it correct?
+        if t.rightp.x == q.x
+            splitting = false
+            tprev = t
         end
 
         # we are building upper and lower division of the current trapezoid
         if !istopactive
             istopactive = true
-            ttop = Trapezoid(t.leftp, t.rightp, t.top, e, lhs(s))
-            ttopd, ttopn = buildnode(ttop)
+            topt, topn = buildnode(t.leftp, t.rightp, t.top, e, lhs(s))
             if prevnodeexists
-                tdprev.rt = ttopd
+                tprev.rt = topt
+                topt.lt = tprev
+            else
+                if t.lt !== nothing
+                    t.lt.rt = topt
+                    topt.lt = t.lt
+                end
             end
         else
-            if ttop.top == t.top
-                ttop.rightp = t.rightp
+            if topt.top == t.top
+                topt.rightp = t.rightp
             else
-                ttop2 = Trapezoid(t.leftp, t.rightp, t.top, e, lhs(s))
-                ttopd2, ttopn = buildnode(ttop2)
-                ttopd.rb = ttopd2
-                ttopd = ttopd2
-                ttop2 = ttop
+                topt2, topn = buildnode(t.leftp, t.rightp, t.top, e, lhs(s))
+                topt.rb = topt2
+                topt2.lb = topt
+                topt = topt2
             end
         end
 
         if !isbotactive
             isbotactive = true
-            tbot = Trapezoid(t.leftp, t.rightp, e, t.bottom, rhs(s))
-            tbotd, tbotn = buildnode(tbot)
+            bott, botn = buildnode(t.leftp, t.rightp, e, t.bottom, rhs(s))
             if prevnodeexists
-                tdprev.rb = tbotd
+                tprev.rb = bott
+                bott.lb = tprev
+            else
+                if t.lb !== nothing
+                    t.lb.rb = bott
+                    bott.lb = t.lb
+                end
             end
         else
-            if tbot.bot == t.bot
-                tbot.rightp = t.rightp
+            if bott.bottom == t.bottom
+                bott.rightp = t.rightp
             else
-                tbot2 = Trapezoid(t.leftp, t.rightp, t.top, e, rhs(s))
-                tbotd2, tbotn = buildnode(tbot2)
-                tbotd.rt = tbotd2
-                tbotd = tbotd2
-                tbot2 = tbot
+                bott2, botn = buildnode(t.leftp, t.rightp, t.top, e, rhs(s))
+                bott.rt = bott2
+                bott2.lt = bott
+                bott = bott2
             end
         end
+        # @info "Divisions" topt bott istopactive isbotactive
 
         node.data.node = nothing
         node.data = nothing
         node.condition = (2, e)
-        node.left = ttopn
-        node.right = tbotn
+        node.left = topn
+        node.right = botn
 
         if splitting
-            if sideof(s, t.rightp)
-                td = td.rt
-            else
-                td = td.rb
-            end
-            t = td.t
-            node = td.node
+            t = findadjacent(s, t)
+            node = t.node
         else
-            ttopd.rt = tdprev
-            tbotd.rb = tdprev
+            topt.rt = tprev
+            bott.rb = tprev
+            tprev.lt = topt
+            tprev.lb = tprev
         end
+        @info "Node data" node t
     end
 end
 
--- test/test01_tzlookup.jl
+++ test/test01_tzlookup.jl
@@ -1,7 +1,24 @@
 module TZLookup
 
 using TimeZoneLookup
+using TimeZoneLookup: buildnode, faceof, E, V
 using Test
+
+@testset "faceof" begin
+    @test faceof(EData(E(V(0, 0), V(0, 10)), -1, 1), V(5, 5)) == 1
+    @test faceof(EData(E(V(0, 0), V(0, 10)), -1, 1), V(-5, 5)) == -1
+    @test faceof(EData(E(V(0, 0), V(10, 0)), -1, 1), V(5, 5)) == -1
+    @test faceof(EData(E(V(0, 0), V(10, 0)), -1, 1), V(5, -5)) == 1
+    @test faceof(EData(E(V(0, 0), V(10, 10)), -1, 1), V(0, 10)) == -1
+    @test faceof(EData(E(V(0, 0), V(10, 10)), -1, 1), V(10, 0)) == 1
+
+    @test faceof(EData(E(V(100, 100), V(100, 110)), -1, 1), V(105, 1055)) == 1
+    @test faceof(EData(E(V(100, 100), V(100, 110)), -1, 1), V(95, 105)) == -1
+    @test faceof(EData(E(V(100, 100), V(110, 100)), -1, 1), V(105, 105)) == -1
+    @test faceof(EData(E(V(100, 100), V(110, 100)), -1, 1), V(105, 95)) == 1
+    @test faceof(EData(E(V(100, 100), V(110, 110)), -1, 1), V(105, 110)) == -1
+    @test faceof(EData(E(V(100, 100), V(110, 110)), -1, 1), V(110, 100)) == 1
+end
 
 @testset "update!" begin
     S = [EData(E(V(10, 10), V(30, 10)), 1, -1),
@@ -9,18 +26,21 @@ using Test
          EData(E(V(30, 10), V(40, 20)), 1, -1),
          EData(E(V(20, 20), V(40, 20)), -1, 1)
         ]
-    bb = Trapezoid(V(0, 0), V(50, 50), E(V(0, 50), V(50, 50)), E(V(0, 0), V(50, 0)), -1)
-    bbdata = TrapezoidData(bb)
-    root = TrapezoidalSearchNode(bbdata)
-    bbdata.node = root
+    bb, root = buildnode(V(0, 0), V(50, 50), E(V(0, 50), V(50, 50)), E(V(0, 0), V(50, 0)), -1)
 
-    s1 = S[2]
-    update!(root, s1)
+    update!(root, S[2])
 
-    @test query(root, V(5, 15)).t.face == -1
-    @test query(root, V(25, 15)).t.face == -1
-    @test query(root, V(15, 5)).t.face == 1
-    @test query(root, V(15, 25)).t.face == -1
+    @test query(root, V(5, 15)).face == -1
+    @test query(root, V(25, 15)).face == -1
+    @test query(root, V(15, 5)).face == 1
+    @test query(root, V(15, 25)).face == -1
+
+    update!(root, S[3])
+
+    @test query(root, V(25, 15)).face == -1
+    @test query(root, V(45, 15)).face == -1
+    @test query(root, V(35, 10)).face == -1
+    @test query(root, V(35, 20)).face == 1
 end
 
 end # module
